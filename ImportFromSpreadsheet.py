03a4daa2a74a25ee797d51e4532eef4e03508f11
Lulu <lulumholdt@gmail.com>
2024-11-10 15:35:51 -0500

    changes for numpy & matplotlib
    I got bored, started learning numpy & matplotlib. Import statement needed to give numpy array instead. Also added very basic code to make a bar graph as an example for us later :D

--- ImportFromSpreadsheet.py
+++ ImportFromSpreadsheet.py
@@ -2,18 +2,26 @@
 # Class: B104
 # Group: Non-Major
 # Project: YRSB Data Analysis- Final Project
-
+import numpy as np
 import pandas as pd
-# import spreadsheet
-dataSet=pd.read_excel("B104_YRBS.xlsx", sheet_name="b104") #import spreadsheet
+import matplotlib.pyplot as plot
 
-# create lists for each relevant column 
-# empty value = nan
-breakfast =dataSet["q75"].to_list() 
-activity =dataSet["q76"].to_list()
-age =dataSet["q1"].to_list()
-sex =dataSet["q2"].to_list()
-height =dataSet["q6"].to_list()
-weight =dataSet["q7"].to_list()
+def makeSexBarGraph(dataSet):
+    """get count of how many participants are male or female"""
+    #(sex == 1) is shorthand if statement, if 1, true, else false.
+    female = (dataSet[:,1] == 1).sum() #sum counts how many entries are true
+    male = (dataSet[:,1] == 2).sum() 
 
+    x= np.array(["females","males"])
+    y=[female,male]
+    plot.bar(x,y) #creates graph
+    plot.show() #shows us the graph
+   
+    #add additional data for people who didn't respond 
+    na= (dataSet[:,1]== "nan").sum() # nan=no response 
+    print(f"{na} people didn't respond. That's {round(na/len(dataSet),2)}% of responses")
 
+# import spreadsheet & convert to numpy 
+dataSet=np.array(pd.read_excel("B104_YRBS.xlsx", sheet_name="b104"))
+
+makeSexBarGraph(dataSet)
